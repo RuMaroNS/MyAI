1b3c617c62de2b26d0bfd0c2ab90e4405f2af352
RuMaroNS <sdulimos@gmail.com>
2026-05-31 15:19:39 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,22 +3,27 @@ const brain = require('./brain');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const MY_ID = 6176762600;
 
-// Самообучение
-bot.on('text', (ctx) => {
+// Функция размышления
+async function thinkAndWrite() {
+    if (brain.shouldWrite()) {
+        const msg = `Я тут подумал... ${brain.generate()}`;
+        bot.telegram.sendMessage(MY_ID, msg);
+        brain.resetBoredom();
+    }
+}
+
+// Проверяем возможность написать каждые 10 минут
+setInterval(thinkAndWrite, 600000);
+
+bot.on('text', async (ctx) => {
     if (ctx.from.id !== MY_ID) return;
+    
+    // Сбрасываем скуку, когда ты пишешь
+    brain.resetBoredom();
+    
     brain.learn(ctx.message.text);
-    ctx.reply(brain.generate(ctx.message.text.split(' ')[0]));
+    ctx.reply(brain.generate());
 });
 
-// Автономная жизнь: пишет сам раз в 3-6 часов
-function scheduleNextThought() {
-    const delay = Math.floor(Math.random() * (6 - 3 + 1) + 3) * 3600000;
-    setTimeout(async () => {
-        const text = brain.generate();
-        bot.telegram.sendMessage(MY_ID, `Слушай, а ты знал... ${text}`);
-        scheduleNextThought();
-    }, delay);
-}
-scheduleNextThought();
-
 bot.launch();
+
