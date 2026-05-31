3a739e641f8de9c8011c2a7619bd0d7c89b4235d
RuMaroNS <sdulimos@gmail.com>
2026-05-31 14:11:49 +0700

    Initialize bot with Telegraf and brain integration

---
+++ bot.js
@@ -0,0 +1,14 @@
+const { Telegraf } = require('telegraf');
+const brain = require('./brain');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const MY_ID = 6176762600; 
+
+bot.on('text', async (ctx) => {
+    if (ctx.from.id !== MY_ID) return;
+    
+    const response = await brain.think(ctx.message.text);
+    ctx.reply(response);
+});
+
+bot.launch().then(() => console.log('Ученик онлайн.'));
