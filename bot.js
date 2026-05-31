ddb405de6d395702d32fcf7563cc571856e58d3d
RuMaroNS <sdulimos@gmail.com>
2026-05-31 14:45:09 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,3 @@
-process.on('uncaughtException', (err) => console.log('Ошибка:', err));
-process.on('unhandledRejection', (err) => console.log('Ошибка промиса:', err));
-
 const { Telegraf } = require('telegraf');
 const brain = require('./brain');
 
@@ -9,12 +6,8 @@ const MY_ID = 6176762600;
 
 bot.on('text', async (ctx) => {
     if (ctx.from.id !== MY_ID) return;
-    try {
-        const response = await brain.think(ctx.message.text);
-        ctx.reply(response);
-    } catch (e) {
-        ctx.reply('Я задумался и чуть не упал, но жив.');
-    }
+    const response = await brain.think(ctx.message.text);
+    ctx.reply(response);
 });
 
 bot.launch();
